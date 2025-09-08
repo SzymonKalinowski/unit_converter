c9e20224c977faf3fbe6b4d0a07bdf7ed36fed64
SzymonKalinowski <u122_szykal_waw@technischools.com>
2025-09-08 09:53:33 +0200

    dodanie modeli

---
+++ src/main/java/org/example/DTO/Category.java
@@ -0,0 +1,10 @@
+package org.example.DTO;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    private String categoryName;
+}
---
+++ src/main/java/org/example/DTO/UnitModel.java
@@ -0,0 +1,16 @@
+package org.example.DTO;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnitModel {
+    private Category category;
+    private String unitName;
+    private String shortName;
+
+
+
+
+}
